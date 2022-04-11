ce9891fca87672e8216c85d2d6d6ca00d1ebc862
rwqasfwefds <rjswo456@naver.com>
2022-04-11 17:47:25 +0900

    Initial commit

---
+++ Ex6/src/ex/Ex6.java
@@ -0,0 +1,25 @@
+package ex;
+
+public class Ex6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Ex6_1
+		Tv t;
+		t = new Tv();
+		t.channel = 7;
+		t.channelDown();
+		System.out.println("현재 채널은 " + t.channel + " 입니다.");
+	}
+
+}
+
+class Tv {
+	String color;
+	boolean power;
+	int channel;
+	
+	void power() {power = !power;}
+	void channelUp() {++channel;}
+	void channelDown() {--channel;}
+}
---
+++ codeup2/src/codeup2/codeup2.java
@@ -0,0 +1,12 @@
+package codeup2;
+
+import java.util.Scanner;
+
+public class codeup2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	}
+
+}
